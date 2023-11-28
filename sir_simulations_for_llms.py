0ac1d418bf16b3cd1bbcb949d21cf85e953276be
Zijian Zhang <121355649+zz242msu@users.noreply.github.com>
2023-11-28 14:26:09 -0600

    Update sir_simulations_for_llms.py

--- sir_simulations_for_llms.py
+++ sir_simulations_for_llms.py
@@ -275,5 +275,5 @@ graphs = [
 
 # Run and save for each graph type 20 times
 for graph_func, graph_name, graph_args in graphs:
-    for run_number in range(1, 2):  # Run 20 times
+    for run_number in range(0, 1):  # Run 20 times
         run_and_save_sir_model(graph_func, graph_name, run_number, graph_args)
